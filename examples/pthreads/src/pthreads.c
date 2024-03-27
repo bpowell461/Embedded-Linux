b9d3cd34dc875800b3018646ba86325bd9bfbf05
bpowell461 <bpowell461@gmail.com>
2024-03-26 19:51:34 -0500

    pthread with CPU affinity

--- examples/pthreads/src/pthreads.c
+++ examples/pthreads/src/pthreads.c
@@ -1,14 +1,17 @@
+#define _GNU_SOURCE
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
 #include <time.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "syslog.h"
+#include "types.h"
 
 #define NUM_THREADS 128
 
-#define ASSIGNMENT 3
+#define ASSIGNMENT 6
 #define COURSE 1
 #define NAME "pthread"
 
@@ -29,8 +32,10 @@ typedef struct
 // POSIX thread declarations and scheduling attributes
 //
 thread_handle_t threads[NUM_THREADS];
-
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+thread_handle_t startThread;
+static pthread_attr_t attr;
+static struct sched_param param;
+static cpu_set_t cpuidx;
 
 
 // Unsafe global
@@ -43,32 +48,52 @@ void* incThread(void* threadp)
     gsum = gsum + 1;
 
     /* Mutex lock is ONLY for uninterrupted trace statements */
-    pthread_mutex_lock(&mutex);
     SYSLOG_TRACE("Thread idx=%i, sum[1...%i]=%i", threadParams->threadIdx, threadParams->threadIdx, gsum);
-    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(DEF_NULL_PTR);
+}
+
+void* bootstrapThread(void* threadp)
+{
+    int i = 0;
+
+    for (i = 0; i < NUM_THREADS; i++)
+    {
+        threads[i].params.threadIdx = (UINT08)(i + 1);
+        pthread_create(&threads[i].thread, &attr, incThread, (void*)&(threads[i].params));
+    }
+
+    for (i = 0; i < NUM_THREADS; i++)
+        pthread_join(threads[i].thread, DEF_NULL_PTR);
+
+    pthread_attr_destroy(&attr);
 
     pthread_exit(DEF_NULL_PTR);
 }
 
 int main(int argc, char* argv[])
 {
-    if (SYS_SUCCESS != syslog_init(NAME, COURSE, ASSIGNMENT))
-        return SYS_FAILURE;
+    syslog_init(NAME, COURSE, ASSIGNMENT);
 
     syslog_printheader();
+     
+    /* Scheduler Setup */
+    pthread_attr_init(&attr);
+    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
 
-    int i = 0;
+    CPU_ZERO(&cpuidx);
+    CPU_SET(0, &cpuidx);
+    pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpuidx);
 
-    for (i = 0; i < NUM_THREADS; i++)
-    {
-        threads[i].params.threadIdx = i+1;
-        pthread_create(&threads[i].thread, DEF_NULL_PTR, incThread, (void*)&(threads[i].params));
-    }
+    param.sched_priority = 50;
 
-    for (i = 0; i < NUM_THREADS; i++)
-        pthread_join(threads[i].thread, DEF_NULL_PTR);
+    sched_setscheduler(getpid(), SCHED_FIFO, &param);
+    pthread_attr_setschedparam(&attr, &param);
 
-    (void) syslog_close();
+    pthread_create(&startThread.thread, &attr, bootstrapThread, DEF_NULL_PTR);
+
+    pthread_join(startThread.thread, DEF_NULL_PTR);
 
     return SYS_SUCCESS;
 }
